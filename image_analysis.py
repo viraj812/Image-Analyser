23ae7c62a44bda0c86248a9d9a4501fea508918c
viraj812 <toviraj08@gmail.com>
2024-05-25 10:01:03 +0530

    updates

--- image_analysis.py
+++ image_analysis.py
@@ -83,47 +83,6 @@ class ImageAnalysis:
             return texts
 
 
-    def show_bounding_boxes(image_bytes, box_sets, colors):
-        """
-        Draws bounding boxes on an image and shows it with the default image viewer.
-
-        :param image_bytes: The image to draw, as bytes.
-        :param box_sets: A list of lists of bounding boxes to draw on the image.
-        :param colors: A list of colors to use to draw the bounding boxes.
-        """
-        image = Image.open(io.BytesIO(image_bytes))
-        draw = ImageDraw.Draw(image)
-        for boxes, color in zip(box_sets, colors):
-            for box in boxes:
-                left = image.width * box["Left"]
-                top = image.height * box["Top"]
-                right = (image.width * box["Width"]) + left
-                bottom = (image.height * box["Height"]) + top
-                draw.rectangle([left, top, right, bottom], outline=color, width=3)
-        image.show()
-
-
-
-    def show_polygons(image_bytes, polygons, color):
-        """
-        Draws polygons on an image and shows it with the default image viewer.
-
-        :param image_bytes: The image to draw, as bytes.
-        :param polygons: The list of polygons to draw on the image.
-        :param color: The color to use to draw the polygons.
-        """
-        image = Image.open(io.BytesIO(image_bytes))
-        draw = ImageDraw.Draw(image)
-        for polygon in polygons:
-            draw.polygon(
-                [
-                    (image.width * point["X"], image.height * point["Y"])
-                    for point in polygon
-                ],
-                outline=color,
-            )
-        image.show()
-
 class ImageLabel:
     """Encapsulates an Amazon Rekognition label."""
 
@@ -359,5 +318,3 @@ def createHTML(labels, text_content, count):
             html_file.write(str(item))
 
     return html_content
-
-# createHTML()
